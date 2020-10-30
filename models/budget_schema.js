dbb96d6f9c20019cf68b0f8ab0dfb2096023c674
unknown <alimendris@gmail.com>
2020-10-30 14:52:58 -0400

    validating hexadecimal color

--- models/budget_schema.js
+++ models/budget_schema.js
@@ -14,12 +14,17 @@ const budget_schema = new mongoose.Schema({
     color: {
         type: String,
         trim: true,
-        minLength: 6,
-        maxLength: 7,
+        min: 6,
         required: true,
-        unique: true
+        unique: true,
+        validate: [hexColorValidation, 'color is not a Hex Color']
         
     }
+    
 }, {collection: 'myBudget'})
+// Reference https://ksloan.net/tips-for-using-mongoose-schemas-with-express-mongo-express-node-stack/
+function hexColorValidation(s) {
+    return /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/.test(s)
+}
 
 module.exports = mongoose.model('myBudget', budget_schema);
